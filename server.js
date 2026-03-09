c97efd71a407a1072922fc5e32004a600a7f96d5
abhishek-071 <abhishekbgp0911@gmail.com>
2026-03-09 19:31:11 +0530

    Backend updated

--- server.js
+++ server.js
@@ -1,23 +1,33 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+
 const taskRoutes = require("./routes/taskRoutes");
-const app = express();
 const authRoutes = require("./routes/authRoutes");
 
+const app = express();
 
+// Connect Database
 connectDB();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/tasks", taskRoutes);
 app.use("/api/auth", authRoutes);
 
+// Test Route
 app.get("/", (req, res) => {
   res.send("Backend is running 🚀");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+// Dynamic Port (important for deployment)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
